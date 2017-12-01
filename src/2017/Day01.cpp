36c6a73cbaa6b033a96147e46bbe6deceb20e55e
ctrlaltf2 <23644849+ctrlaltf2@users.noreply.github.com>
2017-12-01 00:54:07 -0500

    2017 | Day 1 | Part 2

--- src/2017/Day01.cpp
+++ src/2017/Day01.cpp
@@ -2,7 +2,14 @@
 #include <string>
 #include <cstdlib>
 
+void y17day1p2(std::ostream& os, std::istream& is, bool part2);
+
 void y17day1(std::ostream& os, std::istream& is, bool part2) {
+
+    if(part2) {
+        y17day1p2(os, is, part2);
+        return;
+    }
 
     int sum = 0;
     std::string s;
@@ -14,6 +21,20 @@ void y17day1(std::ostream& os, std::istream& is, bool part2) {
                 sum += s.front() - '0';
             }
         } else if(s.at(i) == s.at(i + 1))
+            sum += s.at(i) - '0';
+    }
+
+    os << sum << '\n';
+}
+
+void y17day1p2(std::ostream& os, std::istream& is, bool part2) {
+    int sum = 0;
+    std::string s;
+    is >> s;
+
+    for(int i = 0; i < s.size();++i) {
+        int comp = (i + (s.size() / 2)) % s.size();
+        if(s.at(i) == s.at(comp))
             sum += s.at(i) - '0';
     }
 
