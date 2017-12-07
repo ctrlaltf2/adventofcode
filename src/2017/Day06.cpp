28317ad59261cb8bf31edd6cebe25f87bb704a68
ctrlaltf2 <23644849+ctrlaltf2@users.noreply.github.com>
2017-12-06 20:04:33 -0500

    2017 | Day 6 | [Part 2] Finally caught up

--- src/2017/Day06.cpp
+++ src/2017/Day06.cpp
@@ -1,21 +1,22 @@
 #include <algorithm>
 #include <iostream>
 #include <iterator>
-#include <memory>
-#include <set>
-#include <utility>
+#include <map>
 #include <vector>
 
 void y17day6(std::ostream& os, std::istream& is, bool part2) {
     std::vector<int> bank(std::istream_iterator<int>{is}, {});
 
-    std::set<std::vector<int>> seen;
-    for(int cycles = 0;seen.insert(bank).second;++cycles) {
+    std::map<std::vector<int>, int> seen;
+    for(int cycles = 0;seen.emplace(bank, cycles).second;++cycles) {
         auto max = std::max_element(bank.begin(), bank.end());
         for(int left = std::exchange(*max, 0);left --> 0;++*max)
             if(++max == bank.end())
                 max = bank.begin();
     }
 
-    os << seen.size() << '\n';
+    if(part2)
+        os << seen.size() - seen[bank] << '\n';
+    else
+        os << seen.size() << '\n';
 }
