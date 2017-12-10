ee7a9d6a1fd5cdae2ae40d892e00809f8234c56d
ctrlaltf2 <23644849+ctrlaltf2@users.noreply.github.com>
2017-12-10 01:23:22 -0500

    Added better program options

--- AoC.cpp
+++ AoC.cpp
@@ -1,18 +1,19 @@
-#include "Days.h"
-
 #include <fstream>
 #include <functional>
 #include <iostream>
 #include <map>
 #include <sstream>
 
-void usage(char * progname) {
-    std::cout << "Usage: " << progname << " <year> " << "<day> " << " <part> [eval]\n";
-    std::exit(-1);
-}
+#include <boost/program_options.hpp>
+
+#include "Days.h"
+
+
 
 int main(int argc, char ** argv) {
-    std::map<std::string, std::function<void(std::ostream&, std::istream&, bool)>> funcMap {
+    using namespace boost;
+
+    std::map<std::string, std::function<void(std::ostream&, std::istream&, bool)>> const funcMap {
         {"2016-1", y16day1},
         {"2016-2", y16day2},
         {"2016-3", y16day3},
@@ -22,53 +23,82 @@ int main(int argc, char ** argv) {
         {"2017-4", y17day4},
         {"2017-5", y17day5},
         {"2017-6", y17day6},
-        {"2017-7", y17day7} };
-  
-    std::string mapkey;
-    bool part2 = false, eval = false;
-    if(argc > 1) {
-        if(std::string(argv[1]) == "all")
-            mapkey = "all";
-        else {
-            mapkey = std::string(argv[1]) + '-' + argv[2];
-            if(std::string(argv[3]) == "2")
-                part2 = true;
-            else if(std::string(argv[3]) == "1")
-                ;
-            else
-                usage(argv[0]);
-        }
-    }
-    else
-        usage(argv[0]);
+        {"2017-7", y17day7},
+        {"2017-8", y17day8} };
 
-    if(argc > 4)
-        if(std::string(argv[4]) == "eval")
+    try {
+        program_options::options_description desc{"Options"};
+        desc.add_options()
+            ("help,h", "Help")
+            ("year,y", program_options::value<int>(), "Year")
+            ("day,d", program_options::value<int>(), "Day")
+            ("part,p", program_options::value<std::string>(), "Part")
+            ("eval", "Eval")
+            ("all,a", "All");
+
+        program_options::variables_map vm;
+        program_options::store(
+                program_options::parse_command_line(argc, argv, desc),
+                vm
+        );
+
+        if(vm.count("help")) {
+            std::cout << desc << '\n';
+            return 0;
+        } else if(vm.count("all")) {
+            for(const auto& p : funcMap) {
+                std::ifstream data("data/" + p.first + ".txt");
+                std::cout << p.first << "-part1: ";
+                p.second(std::cout, data, false);
+
+                data.clear();
+                data.seekg(0, std::ios::beg);
+
+                std::cout << p.first << "-part2: ";
+                p.second(std::cout, data, true);
+            }
+            return 0;
+        }
+
+        std::string mapkey;
+
+        if(vm.count("year"))
+            mapkey += std::to_string(vm["year"].as<int>()) + '-';
+
+        if(vm.count("day"))
+            mapkey += std::to_string(vm["day"].as<int>());
+
+        bool eval{false};
+        if(vm.count("eval"))
             eval = true;
 
-    if(mapkey != "all") {
+        std::string part = "both";
+        if(vm.count("part"))
+            part = vm["part"].as<std::string>();
+
         try {
             auto f = funcMap.at(mapkey);
-            if(!eval) {
-                std::ifstream data("data/" + mapkey + ".txt");
-                f(std::cout, data, part2);
-            } else
-                f(std::cout, std::cin, part2);
+            std::ifstream data("data/" + mapkey + ".txt");
+
+            if(part == "1") {
+                f(std::cout, (eval ? std::cin : data), false);
+            } else if(part == "2") {
+                f(std::cout, (eval ? std::cin : data), true);
+            } else if(part == "both") {
+                f(std::cout, (eval ? std::cin : data), false);
+
+                data.clear();
+                data.seekg(0, std::ios::beg);
+
+                f(std::cout, (eval ? std::cin : data), true);
+            }
         } catch(std::out_of_range e) {
-            usage(argv[0]);
+            std::cout << "Invalid year/problem\n";
+            std::cout << desc << '\n';
+            return -1;
         }
-    } else {
-        for(auto& p : funcMap) {
-            std::ifstream data("data/" + p.first + ".txt");
-            std::cout << p.first << "-part1: ";
-            p.second(std::cout, data, false);
-
-            data.clear();
-            data.seekg(0, std::ios::beg);
-
-            std::cout << p.first << "-part2: ";
-            p.second(std::cout, data, true);
-        }
+    } catch (const program_options::error& e) {
+        std::cout << e.what() << '\n';
     }
 }
 
---
+++ src/2017/Day08.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void y17day8(std::ostream& os, std::istream& is, bool part2) {
+
+}
