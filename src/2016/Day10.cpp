10d8dae8285fe7e1b743cc10e5b942aec155147e
ctrlaltf2 <23644849+ctrlaltf2@users.noreply.github.com>
2017-12-12 23:56:31 -0500

    2016 | Day 10 | Synacor prepared me for this...

---
+++ src/2016/Day10.cpp
@@ -0,0 +1,58 @@
+#include <cctype>
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <string>
+#include <vector>
+
+template<typename T>
+T get(std::istream& is) {
+    T t;
+    is >> t;
+    return t;
+}
+
+struct Instruction {
+    std::string op, param1, param2;
+};
+
+void y16day10(std::ostream& os, std::istream& is, bool part2) {
+    std::map<char, int> registers;
+    std::vector<Instruction> memory;
+
+    for(std::string ln;std::getline(is, ln, '\n');) {
+        std::istringstream iss{ln};
+        memory.insert(memory.end(), {{get<std::string>(iss), get<std::string>(iss), get<std::string>(iss)}});
+    }
+
+    int instr_ptr{0};
+    while(instr_ptr < static_cast<int>(memory.size())) {
+        auto& instr = memory[instr_ptr];
+        if(instr.op == "cpy")
+            if(std::isalpha(instr.param1[0]))
+                registers[instr.param2[0]] = registers[instr.param1[0]];
+            else
+                registers[instr.param2[0]] = std::stoi(instr.param1);
+        else if(instr.op == "inc")
+            registers[instr.param1[0]]++;
+        else if(instr.op == "dec")
+            registers[instr.param1[0]]--;
+        else if(instr.op == "jnz") {
+            bool jump{false};
+            if(std::isalpha(instr.param1[0]))
+                jump = registers[instr.param1[0]] != 0;
+            else
+                jump = std::stoi(instr.param1) != 0;
+
+            if(jump) {
+                instr_ptr += std::stoi(instr.param2);
+                continue;
+            }
+        }
+
+        ++instr_ptr;
+    }
+
+    os << registers['a'] << '\n';
+}
+
