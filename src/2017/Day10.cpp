fec93eeb859657c40b996efc35ce1389145142c9
ctrlaltf2 <23644849+ctrlaltf2@users.noreply.github.com>
2017-12-12 19:39:56 -0500

    2017 | Day 10 | [Part 2] Initial Commit

--- src/2017/Day10.cpp
+++ src/2017/Day10.cpp
@@ -1,23 +1,48 @@
 #include <algorithm>
+#include <cstddef>
 #include <iostream>
 #include <iterator>
+#include <iomanip>
 #include <numeric>
 #include <vector>
 
 void y17day10(std::ostream& os, std::istream& is, bool part2) {
 	const int kMax = 256;
-	char ignore;
-	std::vector<int> list(kMax, 0);
+	std::vector<int> list(kMax, 0), input;
 	std::iota(list.begin(), list.end(), 0);
 
 	int pos{0}, skip{0};
-	for(int length; is >> length;) {
-		is >> ignore;
-		std::rotate(list.begin(), std::next(list.begin(), pos), list.end());
-		std::reverse(list.begin(), std::next(list.begin(), length));
-		std::rotate(list.rbegin(), std::next(list.rbegin(), pos), list.rend());
-		pos += length + skip++;
-		pos %= kMax;
+	for(char byte, ignore;is;) {
+		if(part2)
+			is.get(byte);
+		else {
+			int t;
+			is >> t >> ignore;
+			byte = t;
+		}
+		input.push_back(static_cast<int>(static_cast<unsigned char>(byte)));
 	}
-	os << list[0] * list[1] << std::endl;
+	input.erase(std::prev(input.end(), 1));
+	
+	if(part2)
+		input.insert(input.end(), {17, 31, 73, 47, 23});
+	
+	for(int i = 0; i < (part2 ? 64 : 1);++i) {
+		for(auto& length : input) {
+			std::rotate(list.begin(), std::next(list.begin(), pos), list.end());
+			std::reverse(list.begin(), std::next(list.begin(), length));
+			std::rotate(list.rbegin(), std::next(list.rbegin(), pos), list.rend());
+
+			pos += length + skip++;
+			pos %= kMax;
+		}
+	}
+	
+	if(!part2)
+		os << list[0] * list[1] << std::endl;
+	else {
+		for(auto itr = list.begin();itr != list.end();itr += 16) {
+				os << std::setw(2) << std::setfill('0') << std::hex << std::accumulate(itr, itr + 16, 0, std::bit_xor<void>());
+		}
+	}
 }
