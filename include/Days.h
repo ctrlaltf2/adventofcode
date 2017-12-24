8587a2521e143b68095b1daa9426338180793ed7
ctrlaltf2 <23644849+ctrlaltf2@users.noreply.github.com>
2017-12-24 09:17:56 -0500

    2017 | Day 12 | [Part 1] Initial Commit
    Solution came to me in my sleep

--- include/Days.h
+++ include/Days.h
@@ -7,6 +7,7 @@ void y16day1(std::ostream& os, std::istream& is, bool part2);
 void y16day2(std::ostream& os, std::istream& is, bool part2);
 void y16day3(std::ostream& os, std::istream& is, bool part2);
 void y16day12(std::ostream& os, std::istream& is, bool part2);
+void y16day23(std::ostream& os, std::istream& is, bool part2);
 
 // 2017 --------------
 void y17day1(std::ostream& os, std::istream& is, bool part2);
@@ -20,3 +21,4 @@ void y17day8(std::ostream& os, std::istream& is, bool part2);
 void y17day9(std::ostream& os, std::istream& is, bool part2);
 void y17day10(std::ostream& os, std::istream& is, bool part2);
 void y17day11(std::ostream& os, std::istream& is, bool part2);
+void y17day12(std::ostream& os, std::istream& is, bool part2);
---
+++ src/2017/Day12.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <functional>
+#include <map>
+#include <regex>
+#include <sstream>
+#include <vector>
+
+using ProgramID = int;
+using Pipes = std::vector<ProgramID>;
+
+class Village {
+    std::map<ProgramID, Pipes> m_village;
+    std::map<ProgramID, bool> m_seen;
+
+    public:
+        void operator()(std::string line) {
+            std::regex re{R"([^\d]+)"};
+            line = std::regex_replace(line, re, " ");
+
+            std::istringstream iss{line};
+            int id;
+            iss >> id;
+
+            Pipes& connected = m_village[id];
+
+            for(int temp;iss >> temp;)
+                connected.push_back(temp);
+        }
+
+        int part1(int start) {
+            int total{0};
+            m_seen[start] = true;
+            for(const auto& connection : m_village[start]) {
+                if(m_seen[connection] == false) {
+                    ++total;
+                    total += part1(connection);
+
+                    m_seen[connection] = true;
+                }
+            }
+            return total;
+        }
+};
+
+void y17day12(std::ostream& os, std::istream& is, bool part2) {
+    Village ville;
+
+    for(std::string ln;std::getline(is, ln, '\n');)
+        ville(ln);
+
+    std::cout << ville.part1(0) + 1 << '\n';
+
+}
